a6ade7026aa1a373162f8de49d9d52edbcdf6538
wrp801 <wespoulsen@gmail.com>
2022-12-02 15:09:20 -0700

    Added toml for shipyard_rudderstack

--- shipyard_blueprints/__init__.py
+++ shipyard_blueprints/__init__.py
@@ -1,3 +1,2 @@
-# from shipyard_utils import files, args, logs, text, timestamps, webhooks
 import shipyard_utils as utils
-from shipyard_blueprints.etl.rudderstack.shipyard_rudderstack import RudderStack
+from shipyard_blueprints.rudderstack.shipyard_rudderstack import RudderStack
---
+++ shipyard_blueprints/rudderstack/shipyard_rudderstack/test/tests.py
@@ -0,0 +1,46 @@
+# from shipyard_rudderstack import RudderStack
+from shipyard_blueprints import RudderStack
+import unittest
+import os
+
+token = os.environ['RUDDERSTACK_TOKEN']
+source_id = os.environ['RUDDERSTACK_SOURCE']
+
+
+class IncorrectCredentials(unittest.TestCase):
+    def test_credentials(self):
+        rd = RudderStack('bad_token')
+        resp = rd.trigger_sync(source_id)
+        self.assertEqual(resp, rd.EXIT_CODE_INVALID_CREDENTIALS)
+
+
+class InvalidSource(unittest.TestCase):
+    def test_source(self):
+        rd = RudderStack(token)
+        resp = rd.trigger_sync("bad_source")
+        self.assertEqual(resp, rd.EXIT_CODE_SYNC_INVALID_SOURCE_ID)
+
+
+class GoodResponse(unittest.TestCase):
+    """
+    Tests for a correct response. Proper response should be a json object
+    """
+
+    def test_response(self):
+        rd = RudderStack(token)
+        response = rd.get_source_data(source_id)
+        self.assertIsInstance(response, dict)
+
+
+class BadResponse(unittest.TestCase):
+    """ 
+    Tests for an incorrect response. An incorrect response will be an int
+    """
+
+    def test_bad_response(self):
+        rd = RudderStack(token)
+        response = rd.get_source_data("bad_source")
+        self.assertIsInstance(response, int)
+
+
+unittest.main()
