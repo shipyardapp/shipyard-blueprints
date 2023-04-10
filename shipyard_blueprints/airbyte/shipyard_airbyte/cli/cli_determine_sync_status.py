b14a10d6148dac8ee0cb8edf4c040bbbcfc0e5c5
wrp801 <wespoulsen@gmail.com>
2023-04-10 11:17:29 -0600

    Adding clis

---
+++ shipyard_blueprints/airbyte/shipyard_airbyte/cli/cli_determine_sync_status.py
@@ -0,0 +1,33 @@
+from shipyard_airbyte import AirbyteClient
+import shipyard_utils as shipyard 
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api-token', dest = 'api_token', required = True)
+    parser.add_argument('--job-id', dest = 'job_id', default = None ,required = False)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    api_token = args.api_token
+    job_id = args.job_id
+    client = AirbyteClient(api_token)
+    base_folder_name = shipyard.logs.determine_base_artifact_folder(
+        'airbyte')
+    artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+        base_folder_name)
+    shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
+    # check if a job id was provided, otherwise look for it in the artifact folders
+    if job_id is None:
+        response = shipyard.logs.read_pickle_file(artifact_subfolder_paths, 'sync_response')
+        job_id = response['job_id']
+    job_status = client.get_sync_status(job_id= job_id)
+    sync_status = client.determine_sync_status(job_status)
+    sys.exit(sync_status)
+        
+
+if __name__ == "__main__":
+    main()
---
+++ shipyard_blueprints/airbyte/shipyard_airbyte/cli/cli_trigger_sync.py
@@ -0,0 +1,33 @@
+import argparse
+from shipyard_airbyte import AirbyteClient
+import shipyard_utils as shipyard
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--connection-id", dest = 'connection_id', required = True)
+    parser.add_argument("--api-token", dest = 'api_token', required = True)
+    parser.add_argument('--check_status', dest = 'check_status', required = False)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    connection_id = args.connection_id 
+    check_status = args.check_status
+    api_token = args.api_token
+    client = AirbyteClient(access_token= api_token)
+    resp = client.trigger_sync(connection_id= connection_id, check_status= check_status)
+    # create artifacts folder to save response
+    base_folder_name = shipyard.logs.determine_base_artifact_folder(
+        'airbyte')
+    artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+        base_folder_name)
+    shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
+
+    # save sync response as variable
+    shipyard.logs.create_pickle_file(artifact_subfolder_paths,
+                                     'sync_response', resp)
+
+if __name__ == "__main__":
+    main()
