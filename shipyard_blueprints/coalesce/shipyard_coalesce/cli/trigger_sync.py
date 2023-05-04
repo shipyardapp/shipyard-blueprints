be14752715c9fcd0a1d80223445953dc80577809
wrp801 <wespoulsen@gmail.com>
2023-05-04 15:26:23 -0600

    changes per review

--- shipyard_blueprints/coalesce/shipyard_coalesce/cli/trigger_sync.py
+++ shipyard_blueprints/coalesce/shipyard_coalesce/cli/trigger_sync.py
@@ -4,49 +4,61 @@ from requests import get
 from shipyard_coalesce import CoalesceClient
 import shipyard_bp_utils as shipyard
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--access-token', dest = 'access_token',
-                        required= True)
-    parser.add_argument('--environment-id', dest = 'enviornment_id', required = True)
-    parser.add_argument('--job-id', dest = 'job_id', required = False, default= None)
-    parser.add_argument('--snowflake-username',dest = 'snowflake_username' ,required = True)
-    parser.add_argument('--snowflake-password', dest = 'snowflake_password', required = True)
-    parser.add_argument('--snowflake-role', dest = 'snowflake_role', required = True)
-    parser.add_argument('--snowflake-warehouse', dest = 'snowflake_warehouse', required = True)
-    parser.add_argument('--parallelism', dest = 'parallelism', required= False, default = 16)
-    parser.add_argument('--include-nodes-selector', dest = 'include_nodes', required = False, default = None)
-    parser.add_argument('--exclude-nodes-selector', dest = 'exclude_nodes', required = False, default = None)
+    parser.add_argument("--access-token", dest="access_token", required=True)
+    parser.add_argument("--environment-id", dest="environment_id", required=True)
+    parser.add_argument("--job-id", dest="job_id", required=False, default=None)
+    parser.add_argument(
+        "--snowflake-username", dest="snowflake_username", required=True
+    )
+    parser.add_argument(
+        "--snowflake-password", dest="snowflake_password", required=True
+    )
+    parser.add_argument("--snowflake-role", dest="snowflake_role", required=True)
+    parser.add_argument(
+        "--snowflake-warehouse", dest="snowflake_warehouse", required=True
+    )
+    parser.add_argument("--parallelism", dest="parallelism", required=False, default=16)
+    parser.add_argument(
+        "--include-nodes-selector", dest="include_nodes", required=False, default=None
+    )
+    parser.add_argument(
+        "--exclude-nodes-selector", dest="exclude_nodes", required=False, default=None
+    )
 
     args = parser.parse_args()
     return args
 
+
 def main():
     args = get_args()
     access_token = args.access_token
-    environment_id = args.enviornment_id
-    job_id = args.job_id
-    snowflake_username = args.snowflake_username
-    snowflake_password = args.snowflake_password
-    snowflake_role = args.snowflake_role
-    snowflake_warehouse = args.snowflake_warehouse
-    parallelism = args.parallelism
-    include_nodes = args.include_nodes
-    exclude_nodes = args.exclude_nodes
+    sync_args = {
+        "environment_id": args.environment_id,
+        "job_id": args.job_id,
+        "snowflake_username": args.snowflake_username,
+        "snowflake_password": args.snowflake_password,
+        "snowflake_role": args.snowflake_role,
+        "snowflake_warehouse": args.snowflake_warehouse,
+        "parallelism": args.parallelism,
+        "include_nodes_selector": args.include_nodes,
+        "exclude_nodes_selector": args.exclude_nodes,
+    }
     client = CoalesceClient(access_token)
-    response = client.trigger_sync(environment_id = environment_id, job_id = job_id, snowflake_username = snowflake_username,
-                            snowflake_password = snowflake_password, snowflake_warehouse = snowflake_warehouse,
-                            snowflake_role = snowflake_role, parallelism = parallelism, include_nodes_selector = include_nodes, exclude_nodes_selector = exclude_nodes)
+    response = client.trigger_sync(**sync_args)
 
     # create artifacts folder to save response
-    base_folder_name = shipyard.logs.determine_base_artifact_folder(
-        'coalesce')
+    base_folder_name = shipyard.logs.determine_base_artifact_folder("coalesce")
     artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
-        base_folder_name)
+        base_folder_name
+    )
     shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
     # save sync response as variable
-    shipyard.logs.create_pickle_file(artifact_subfolder_paths,
-                                     'coalesce_response', response)
+    shipyard.logs.create_pickle_file(
+        artifact_subfolder_paths, "coalesce_response", response
+    )
 
 
 if __name__ == "__main__":
--- shipyard_blueprints/coalesce/shipyard_coalesce/coalesce.py
+++ shipyard_blueprints/coalesce/shipyard_coalesce/coalesce.py
@@ -1,15 +1,26 @@
 import requests
 from shipyard_templates import Etl
 
+
 class CoalesceClient(Etl):
-    def __init__(self,access_token: str):
+    def __init__(self, access_token: str):
         self.base_url = "https://app.coalescesoftware.io/scheduler"
         super().__init__(access_token)
 
-    def trigger_sync(self, environment_id:str, job_id:str, snowflake_username:str, snowflake_password:str, snowflake_role:str, snowflake_warehouse:str = None, 
-                     parallelism:int = 16, include_nodes_selector:str = None, exclude_nodes_selector = None) -> dict[any,any]:
+    def trigger_sync(
+        self,
+        environment_id: str,
+        job_id: str,
+        snowflake_username: str,
+        snowflake_password: str,
+        snowflake_role: str,
+        snowflake_warehouse: str = None,
+        parallelism: int = 16,
+        include_nodes_selector: str = None,
+        exclude_nodes_selector=None,
+    ) -> dict[any, any]:
         """
-
+        # reference is available here: https://docs.coalesce.io/reference/startrun
         Args:
             exclude_nodes_selector (): nodes excluded for a job
             environment_id:  the environment being refreshed
@@ -22,40 +33,37 @@ class CoalesceClient(Etl):
             include_nodes_selector: nodes included for a job
 
         Returns: HTTP response from the API
-            
+
         """
-        # reference is available here: https://docs.coalesce.io/reference/startrun
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "Authorization": f"Bearer {self.access_token}"}
-        url = f'{self.base_url}/startRun'
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        url = f"{self.base_url}/startRun"
         # populate the inner payload for the userCredentials object
         credentials = {
-                "snowflakeUsername": snowflake_username,
-                "snowflakePassword": snowflake_password,
-                "snowflakeRole": snowflake_role,
-                "snowflakeAuthType": "Basic"
-                }
-        if snowflake_warehouse is not None:
-            credentials['snowflakeWarehouse'] = snowflake_warehouse
+            "snowflakeUsername": snowflake_username,
+            "snowflakePassword": snowflake_password,
+            "snowflakeRole": snowflake_role,
+            "snowflakeAuthType": "Basic",
+        }
+        if snowflake_warehouse:
+            credentials["snowflakeWarehouse"] = snowflake_warehouse
         # populate the inner payload for the runDetails object
         # environmentId is required and parallelism has a default value
-        details = {"environmentID": environment_id,
-                   'parallelism': parallelism}
+        details = {"environmentID": environment_id, "parallelism": parallelism}
         # jobId, and include/exclude_nodes_selector are optional
-        if job_id is not None:
-            details['jobId'] = job_id
-        if include_nodes_selector is not None:
-            details['includeNodesSelector'] = include_nodes_selector
-        if exclude_nodes_selector is not None:
-            details['excludeNodesSelector'] = exclude_nodes_selector
+        if job_id:
+            details["jobId"] = job_id
+        if include_nodes_selector:
+            details["includeNodesSelector"] = include_nodes_selector
+        if exclude_nodes_selector:
+            details["excludeNodesSelector"] = exclude_nodes_selector
 
+        payload = {"runDetails": details, "userCredentials": credentials}
 
-        payload = {'runDetails' : details, 
-                   'userCredentials' : credentials}
-
-        response = requests.post(url = url, json = payload, headers = headers)
+        response = requests.post(url=url, json=payload, headers=headers)
 
         if response.status_code == 200:
             self.logger.info("Successfully triggered job")
@@ -66,9 +74,8 @@ class CoalesceClient(Etl):
 
         return response.json()
 
-
-    def get_run_status(self, run_counter:int) -> requests.Response: 
-        """ Returns the HTTP response for a Coalesce job status
+    def get_run_status(self, run_counter: int) -> requests.Response:
+        """Returns the HTTP response for a Coalesce job status
 
         Args:
             run_counter (int): The specific run in which to fetch the status
@@ -78,15 +85,14 @@ class CoalesceClient(Etl):
         """
         url = f"{self.base_url}/runStatus?runCounter={run_counter}"
         headers = {
-                "accept": "application/json",
-                "Authorization": f"Bearer {self.access_token}"
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
         }
-        response = requests.get(url = url, headers = headers)
+        response = requests.get(url=url, headers=headers)
         return response
 
-
-    def determine_sync_status(self, run_counter:int) -> int:
-        """ Analyzes the statuses returned from the API and returns an exit code based on that for the Shipyard application
+    def determine_sync_status(self, run_counter: int) -> int:
+        """Analyzes the statuses returned from the API and returns an exit code based on that for the Shipyard application
 
         Args:
             run_counter (int): The specific run in which to fetch the status
@@ -97,22 +103,22 @@ class CoalesceClient(Etl):
         response = self.get_run_status(run_counter)
         json = response.json()
         if response.status_code == 200:
-            status = json.get('runStatus')
+            status = json.get("runStatus")
             # go through the statuses and return the appropriate exit code
             # documentation does not provide options for status aside from completed
-            if status == 'completed':
+            if status == "completed":
                 self.logger.info(f"Status: completed")
                 return self.EXIT_CODE_FINAL_STATUS_COMPLETED
-            elif status == 'pending':
+            elif status == "pending":
                 self.logger.info("Status: pending")
                 return self.EXIT_CODE_FINAL_STATUS_PENDING
-            elif status == 'running':
+            elif status == "running":
                 self.logger.info("Status: running")
                 return self.EXIT_CODE_SYNC_ALREADY_RUNNING
-            elif status == 'timeout':
+            elif status == "timeout":
                 self.logger.info("Status: timeout")
                 return self.EXIT_CODE_FINAL_STATUS_INCOMPLETE
-            elif status == 'canceled':
+            elif status == "canceled":
                 self.logger.info("Status: canceled")
                 return self.EXIT_CODE_FINAL_STATUS_CANCELLED
             else:
@@ -120,17 +126,13 @@ class CoalesceClient(Etl):
                 return self.EXIT_CODE_UNKNOWN_STATUS
 
         elif response.status_code == 400:
-            self.logger.error(f"There was an error when attempting to fetch the status of the job. The message returned from the API is {response['error']['errorString']}")
+            self.logger.error(
+                f"There was an error when attempting to fetch the status of the job. The message returned from the API is {response['error']['errorString']}"
+            )
             return self.EXIT_CODE_BAD_REQUEST
-        
+
         elif response.status_code == 401:
-            self.logger.error("Error occurred when attempting to authenticate, please ensure that the token provided is valid")
+            self.logger.error(
+                "Error occurred when attempting to authenticate, please ensure that the token provided is valid"
+            )
             return self.EXIT_CODE_INVALID_CREDENTIALS
-
-
-
-
-
-
-
-
