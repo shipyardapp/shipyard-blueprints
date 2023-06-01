5a60c6a3558353d7c3a7bfdffb33f1210a1e2497
wrp801 <wespoulsen@gmail.com>
2023-06-01 08:50:16 -0600

    added clickup

--- shipyard_blueprints/clickup/shipyard_clickup/clickup.py
+++ shipyard_blueprints/clickup/shipyard_clickup/clickup.py
@@ -1,1 +1,21 @@
 import requests
+from shipyard_templates import ProjectManagement
+
+class ClickupClient(ProjectManagement):
+    def __init__(self, access_token: str) -> None:
+        self.access_token = access_token
+        super().__init__(access_token)
+
+
+    def connect():
+        pass
+
+    def create_ticket():
+        pass
+
+    def get_ticket():
+        pass
+
+    def update_ticket():
+        pass
+
