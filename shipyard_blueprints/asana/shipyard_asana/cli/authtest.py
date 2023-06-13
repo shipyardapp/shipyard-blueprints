1c3f7200c2bf24d7667912b89f538a9d2a5795e8
wrp801 <wespoulsen@gmail.com>
2023-06-13 15:20:25 -0600

    Removed unnecessary function

--- shipyard_blueprints/asana/shipyard_asana/cli/authtest.py
+++ shipyard_blueprints/asana/shipyard_asana/cli/authtest.py
@@ -1,16 +1,9 @@
 import requests
 import os
 
-def get_args():
-    args = {}
-    args['access_token'] = os.getenv('ASANA_ACCESS_TOKEN')
-    return args
-
 def main():
-    args = get_args()
-    token = args['access_token']
     url = 'https://app.asana.com/api/1.0/users/me'
-    headers = {"Authorization": f"Bearer {token}" }
+    headers = {"Authorization": f"Bearer {os.getenv('ASANA_ACCESS_TOKEN')}" }
     resp = requests.get(url = url, headers=  headers)
     if resp.status_code == 200:
         return 0
