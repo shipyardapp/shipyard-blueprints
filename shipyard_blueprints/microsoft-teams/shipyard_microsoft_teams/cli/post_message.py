07091bdc71a0bbc1a32d121f81eb7c488a53f958
Johnathan Rodriguez <johnathan.rodriguez@shipyardapp.com>
2023-06-14 09:25:34 -0400

    Included logging for error and formatted the code with black

--- shipyard_blueprints/microsoft-teams/shipyard_microsoft_teams/cli/post_message.py
+++ shipyard_blueprints/microsoft-teams/shipyard_microsoft_teams/cli/post_message.py
@@ -1,24 +1,44 @@
 import argparse
 import sys
-
+import logging
 import requests
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Send a message to Microsoft Teams using an incoming webhook')
-    parser.add_argument('--webhook-url', required=True, help='URL of the incoming webhook')
-    parser.add_argument('--message-content', required=True, help='Content of the message to be sent')
-    parser.add_argument('--message-title', required=False, help='Title of the message to be sent')
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
+
+    parser = argparse.ArgumentParser(
+        description="Send a message to Microsoft Teams using an incoming webhook"
+    )
+    parser.add_argument(
+        "--webhook-url", required=True, help="URL of the incoming webhook"
+    )
+    parser.add_argument(
+        "--message-content", required=True, help="Content of the message to be sent"
+    )
+    parser.add_argument(
+        "--message-title", required=False, help="Title of the message to be sent"
+    )
     args = parser.parse_args()
 
-    response = requests.post(args.webhook_url, json={'title': args.message_title, 'text': args.message_content})
+    response = requests.post(
+        args.webhook_url,
+        json={"title": args.message_title, "text": args.message_content},
+    )
 
     if response.ok:
-        print('Message sent successfully')
+        logging.info("Message sent successfully")
     else:
-        print('Message failed to send')
+        logging.error(
+            f"An error occurred when attempting to post message to Microsoft teams.\n"
+            f"{response.text}"
+        )
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
