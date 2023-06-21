5beaff5a84acf6ee86e0ca031458186cef21875e
Johnathan Rodriguez <johnathan.rodriguez@shipyardapp.com>
2023-06-21 10:27:06 -0400

    Fivetran Library Code [sc-5557]

--- shipyard_blueprints/fivetran/shipyard_fivetran/cli/authtest.py
+++ shipyard_blueprints/fivetran/shipyard_fivetran/cli/authtest.py
@@ -1,23 +1,13 @@
-import os 
-from shipyard_blueprints import FivetranClient
-
-
-def get_args():
-    args = {}
-    args['api_key'] = os.getenv('FIVETRAN_API_KEY')
-    args['api_secret'] = os.getenv('FIVETRAN_API_SECRET')
-    return args
+import os
+from shipyard_fivetran import FivetranClient
 
 
 def main():
-    args = get_args()
-    client = FivetranClient(args['api_key'], args['api_secret'])
-    resp = client.connect()
-    if resp == 200:
-        return 0
-    else:
-        return 1
+    return FivetranClient(
+        access_token=os.getenv("FIVETRAN_API_KEY"),
+        api_secret=os.getenv("FIVETRAN_API_SECRET"),
+    ).connect()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
