2a101d04c9fff8008003e9fe1dfa6742a4713203
wrp801 <wespoulsen@gmail.com>
2023-07-14 11:55:16 -0600

    updated athena credential test

--- shipyard_blueprints/athena/shipyard_athena/cli/authtest.py
+++ shipyard_blueprints/athena/shipyard_athena/cli/authtest.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-from shipyard_blueprints import AthenaClient
+import boto3
+from shipyard_athena import AthenaClient
 
 
 def get_args():
@@ -11,31 +12,21 @@ def get_args():
     return args
 
 def main():
-    # args = get_args()
-    # access_key = args['aws_access_key']
-    # secret_key = args['aws_secret_key']
-    # region = args['region']
-
-    client = AthenaClient(os.getenv('AWS_ACCESS_KEY_ID'), os.getenv('AWS_SECRET_ACCESS_KEY'))
+    athena = AthenaClient(os.getenv('AWS_ACCESS_KEY_ID'), os.getenv('AWS_SECRET_ACCESS_KEY'))
 
     try:
         client = boto3.client('sts', aws_access_key_id= os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY'))
         response = client.get_caller_identity()
-        if response['ReponseMetadata']['HTTPStatusCode'] == 200:
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            athena.logger.info("Successfully connected to AWS Athena")
             return 0
     except Exception as e:
-        s3.logger.error(f"Could not connect to the AWS Athena")
+        athena.logger.error(f"Could not connect to the AWS Athena")
         return 1
     
     else:
-        s3.logger.error(f"Could not connect to the AWS Athena")
+        athena.logger.error(f"Could not connect to the AWS Athena")
         return 1
-
-
-
-    
-
-
 
 if __name__ == '__main__':
     main()
