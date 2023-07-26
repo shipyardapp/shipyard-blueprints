c25084bd7f2eca3444b27d633ab9614c5c20c019
wrp801 <wespoulsen@gmail.com>
2023-07-26 14:38:21 -0600

    testing exit vs return values

--- shipyard_blueprints/dbt/shipyard_dbt/cli/authtest.py
+++ shipyard_blueprints/dbt/shipyard_dbt/cli/authtest.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from shipyard_blueprints import DbtClient
 
 
@@ -6,7 +7,8 @@ def main():
     client = DbtClient(access_token = os.getenv('DBT_API_KEY'),
                        account_id = os.getenv('DBT_ACCOUNT_ID'))
 
-    return client.connect()
+    sys.exit(client.connect())
+    
 
 if __name__ == '__main__':
     main()
