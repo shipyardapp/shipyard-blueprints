b9c4828a3eacf9d8a8d0d84bf62d417e51b60120
wrp801 <wespoulsen@gmail.com>
2023-07-26 10:18:19 -0600

    updated redshift to not use SQLAlchemy

--- shipyard_blueprints/redshift/tests/test_auth.py
+++ shipyard_blueprints/redshift/tests/test_auth.py
@@ -14,4 +14,50 @@ def conn_helper(client:RedshiftClient) -> int:
 
 
 def test_good_connection():
-    pass
+    client = RedshiftClient(host = os.getenv('REDSHIFT_HOST'),
+                            pwd = os.getenv('REDSHIFT_PASSWORD'),
+                            database = os.getenv('REDSHIFT_DATABASE'),
+                            user = os.getenv('REDSHIFT_USERNAME'),
+                            port = os.getenv('REDSHIFT_PORT'))
+
+    assert conn_helper(client) == 0
+
+
+def test_bad_host():
+    client = RedshiftClient(host = os.getenv('bad_host'),
+                            pwd = os.getenv('REDSHIFT_PASSWORD'),
+                            database = os.getenv('REDSHIFT_DATABASE'),
+                            user = os.getenv('REDSHIFT_USERNAME'),
+                            port = os.getenv('REDSHIFT_PORT'))
+
+    assert conn_helper(client) == 1
+
+def test_bad_pwd():
+    client = RedshiftClient(host = os.getenv('REDSHIFT_HOST'),
+                            pwd = os.getenv('bad_password'),
+                            database = os.getenv('REDSHIFT_DATABASE'),
+                            user = os.getenv('REDSHIFT_USERNAME'),
+                            port = os.getenv('REDSHIFT_PORT'))
+
+    assert conn_helper(client) == 1
+
+def test_bad_user():
+    client = RedshiftClient(host = os.getenv('REDSHIFT_HOST'),
+                            pwd = os.getenv('REDSHIFT_PASSWORD'),
+                            database = os.getenv('REDSHIFT_DATABASE'),
+                            user = os.getenv('bad_user'),
+                            port = os.getenv('REDSHIFT_PORT'))
+
+    assert conn_helper(client) == 1
+
+def test_bad_database():
+    client = RedshiftClient(host = os.getenv('REDSHIFT_HOST'),
+                            pwd = os.getenv('REDSHIFT_PASSWORD'),
+                            database = os.getenv('bad_database'),
+                            user = os.getenv('REDSHIFT_USERNAME'),
+                            port = os.getenv('REDSHIFT_PORT'))
+
+    assert conn_helper(client) == 1
+
+
+
