e486cec93810a4cbae665cc249278cf2f683cc94
wrp801 <wespoulsen@gmail.com>
2023-07-26 09:11:39 -0600

    added googlesheets auth tests

---
+++ shipyard_blueprints/googlesheets/tests/test_auth.py
@@ -0,0 +1,28 @@
+import os
+from shipyard_googlesheets import GoogleSheetsClient
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+def conn_helper(client: GoogleSheetsClient) -> int:
+    try:
+        service, drive_service = client.connect()
+        client.logger.info("Successfully connected to Google Sheets")
+        return 0
+    except Exception as e:
+        client.logger.error('Could not connect to Google Sheets')
+        return 1
+
+
+def test_good_connection():
+    client = GoogleSheetsClient(service_account = os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+
+    assert conn_helper(client) == 0
+
+
+def test_bad_connection():
+    client = GoogleSheetsClient(service_account = 'bad_credentials')
+
+    assert conn_helper(client) == 1
+
