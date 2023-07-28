2dd51c3f3f56d8a4beb4df9d731ef8de9e41d98b
Johnathan Rodriguez <johnathan.rodriguez@shipyardapp.com>
2023-07-28 17:28:14 -0400

    Adjust the cli command to allow for the wait for completion option

--- shipyard_blueprints/airbyte/shipyard_airbyte/cli/cli_trigger_sync.py
+++ shipyard_blueprints/airbyte/shipyard_airbyte/cli/cli_trigger_sync.py
@@ -1,4 +1,6 @@
 import argparse
+import sys
+import time
 from shipyard_airbyte import AirbyteClient
 import shipyard_bp_utils as shipyard
 
@@ -7,18 +9,12 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--connection-id", dest='connection_id', required=True)
     parser.add_argument("--api-token", dest='api_token', required=True)
-    args = parser.parse_args()
-    return args
+    parser.add_argument("--wait-for-completion", dest='wait_for_completion')
+    parser.add_argument("--poke-interval", dest='poke_interval')
+    return parser.parse_args()
 
 
-def main():
-    args = get_args()
-    connection_id = args.connection_id
-    api_token = args.api_token
-    client = AirbyteClient(access_token=api_token)
-    resp = client.trigger_sync(
-        connection_id=connection_id)
-    # create artifacts folder to save response
+def generate_pickle_file(job_id):
     base_folder_name = shipyard.logs.determine_base_artifact_folder(
         'airbyte')
     artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
@@ -27,7 +23,40 @@ def main():
 
     # save sync response as variable
     shipyard.logs.create_pickle_file(artifact_subfolder_paths,
-                                     'sync_response', resp['jobId'])
+                                     'sync_response', job_id)
+
+
+def main():
+    args = get_args()
+    connection_id = args.connection_id
+    api_token = args.api_token
+
+    client = AirbyteClient(access_token=api_token)
+    try:
+        resp = client.trigger_sync(connection_id=connection_id)
+    except Exception as e:
+        client.logger.error(
+            f"Error occurred when attempting to trigger sync due to the following error: {e}")
+        sys.exit(1)
+
+    job_id = resp['jobId']
+
+    generate_pickle_file(job_id)  # This is for backwards compatibility the Check Sync Blueprint needs in order to run
+
+    if args.wait_for_completion == 'TRUE' and (0 < args.poke_interval <= 60):
+        status = client.determine_sync_status(job_id=job_id)
+        while status not in (client.EXIT_CODE_FINAL_STATUS_COMPLETED,
+                             client.EXIT_CODE_FINAL_STATUS_INCOMPLETE,
+                             client.EXIT_CODE_FINAL_STATUS_CANCELLED):
+            status = client.determine_sync_status(job_id=job_id)
+            time.sleep(args.poke_interval)
+
+        sys.exit(status)
+    elif args.wait_for_completion == 'TRUE':
+        client.logger.error(
+            "Poke interval must be between 1 and 60 seconds")
+        sys.exit(client.EXIT_CODE_SYNC_INVALID_POKE_INTERVAL)
+
 
 
 if __name__ == "__main__":
