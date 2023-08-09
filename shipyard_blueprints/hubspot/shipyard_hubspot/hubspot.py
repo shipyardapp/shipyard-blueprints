9b00f38ea77f2b235e4fffc46442af1e3dae43d7
Johnathan Rodriguez <johnathan.rodriguez@shipyardapp.com>
2023-08-09 15:11:24 -0400

    testing the set output

--- shipyard_blueprints/hubspot/shipyard_hubspot/hubspot.py
+++ shipyard_blueprints/hubspot/shipyard_hubspot/hubspot.py
@@ -74,6 +74,7 @@ class HubspotClient(Crm):
             raise ExitCodeException(response.text, self.EXIT_CODE_UNKNOWN_ERROR)
 
     def connect(self):
+
         """
         Method for verifying connection to the Hubspot API
         """
