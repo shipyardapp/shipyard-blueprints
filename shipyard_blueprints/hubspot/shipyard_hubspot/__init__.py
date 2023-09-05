c485608c34b04bb1e2a3c46500da336b69696384
Johnathan Rodriguez <johnathan.rodriguez@shipyardapp.com>
2023-09-05 16:00:50 -0400

    Updated export file name

--- shipyard_blueprints/hubspot/shipyard_hubspot/__init__.py
+++ shipyard_blueprints/hubspot/shipyard_hubspot/__init__.py
@@ -1,5 +1,5 @@
 from .hubspot import HubspotClient
 from .cli import authtest
 from .cli import csv_headers_assist
-from .cli import export_contacts
+from .cli import export_list
 from .cli import upload_data
