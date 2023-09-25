a9edd80feacb348ece57dd3a23c8fa6331f04ef1
wrp801 <wespoulsen@gmail.com>
2023-09-24 20:30:23 -0600

    adding notion cli commands

--- shipyard_blueprints/notion/shipyard_notion/cli/upload.py
+++ shipyard_blueprints/notion/shipyard_notion/cli/upload.py
@@ -1,1 +1,60 @@
 import os
+import logging
+import argparse
+from notion.client import NotionClient
+
+
+
+def get_args():
+    pass
+
+def get_logger():
+    logger = logging.getLogger("Shipyard")
+    logger.setLevel(logging.DEBUG)
+    # Add handler for stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    # add specific format
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s -%(lineno)d: %(message)s"
+    )
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+    return logger
+
+def connect(access_token:str, logger:logging.Logger):
+    """ Helper function to establish a connection to Notion
+
+    Args:
+        access_token: The personal access token created for the Notion API
+        logger: The logger to print feedback to STDOUT
+
+    Returns: The Notion Client
+        
+    """
+    try:
+        client = NotionClient(auth = access_token)
+    except Exception as e:
+        logger.error(f"Error in trying to connect to Notion")
+        raise e
+    else:
+        return client
+
+
+def upload(access_token:str, logger:logging.Logger ,table_name:str):
+    """ 
+
+    Args:
+        access_token: 
+        table_name: 
+    """
+    client = connect(access_token, logger)
+
+    client.databases.create(title = table_name)
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- shipyard_blueprints/notion/shipyard_notion/notion.py
+++ shipyard_blueprints/notion/shipyard_notion/notion.py
@@ -1,6 +1,41 @@
 import pandas as pd
-from notion import NotionClient
+from notion.client import NotionClient
+from sqlalchemy import except_
+
+class ShipyardNotion():
+
+    def __init__(self, access_token:str) -> None:
+        self.access_token = access_token
+
+    def connect(self):
+        return NotionClient(auth = self.access_token)
+
+    def upload(self):
+        pass
+
+    def download(self):
+        pass
+
+def connect(access_token:str):
+    """ Establishes a connection client with notion
+
+    Args:
+        access_token: The access token generated in Notion for programatic access
+
+    Returns: The notion client
+        
+    """
+    return NotionClient(auth = access_token)
+
+def upload(client:NotionClient, data:pd.DataFrame):
+    pass
 
 
-def connect(token:str):
-    return NotionClient(token)
+def download(client:NotionClient):
+    pass
+
+
+
+    
+    
+
