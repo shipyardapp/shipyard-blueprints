fb72639b8de02d4a53d6af6aba59a35ec51ae22f
wrp801 <wespoulsen@gmail.com>
2023-10-06 20:45:56 -0600

    added methods to spreadsheets

--- shipyard-templates/shipyard_templates/spreadsheets.py
+++ shipyard-templates/shipyard_templates/spreadsheets.py
@@ -3,6 +3,11 @@ from .shipyard_logger import ShipyardLogger
 
 
 class Spreadsheets(ABC):
+    EXIT_CODE_INVALID_TOKEN = 200 
+    EXIT_CODE_INVALID_DATABASE_ID = 201
+    EXIT_CODE_UPLOAD_ERROR = 202    
+    EXIT_CODE_DOWNLOAD_ERROR = 203
+    EXIT_CODE_UNKNWON_ERROR = 299
     def __init__(self, **kwargs) -> None:
         self.logger = ShipyardLogger().logger
         super().__init__()
@@ -10,3 +15,12 @@ class Spreadsheets(ABC):
     @abstractmethod
     def connect(self):
         pass
+
+    @abstractmethod 
+    def upload(self):
+        pass
+
+    @abstractmethod 
+    def download(self):
+        pass
+
