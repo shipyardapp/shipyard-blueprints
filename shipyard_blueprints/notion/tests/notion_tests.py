7b70384d1114bc65e816e2e6b6fa8892679d854c
wrp801 <wespoulsen@gmail.com>
2023-10-19 07:23:53 -0600

    reformatted tests

--- shipyard_blueprints/notion/tests/notion_tests.py
+++ shipyard_blueprints/notion/tests/notion_tests.py
@@ -5,35 +5,40 @@ import json
 from shipyard_notion import NotionClient, create_row_payload
 from dotenv import load_dotenv, find_dotenv
 
+if env_exists := os.path.exists(".env"):
+    load_dotenv()
 
-load_dotenv(find_dotenv())
-token = str(os.getenv("NOTION_ACCESS_TOKEN"))
-db = os.getenv("NOTION_DB")
-client = NotionClient(token)
-try:
-    df = pd.read_csv("test_sample.csv")
-except:
-    df = pd.read_csv("tests/test_sample.csv")
 
+@pytest.fixture
+def token():
+    return str(os.getenv("NOTION_ACCESS_TOKEN"))
+
+
+@pytest.fixture
+def db():
+    return os.getenv("NOTION_DB")
+
+
+@pytest.fixture
+def client():
+    return NotionClient(token)
+
+
+@pytest.mark.skipif(not env_exists, reason="no .env file")
 def test_upload():
     data = client.client.databases.query(database_id=db)
     client.upload(database_id=db, data=df, insert_method="append")
 
 
+@pytest.mark.skipif(not env_exists, reason="no .env file")
 def test_upload_replace():
     data = client.client.databases.query(database_id=db)
     client.upload(database_id=db, data=df, insert_method="replace")
 
 
+@pytest.mark.skipif(not env_exists, reason="no .env file")
 def test_fetch():
     data = client.client.databases.query(database_id=db)
     with open("notion_data.json", "w") as f:
         json.dump(data, f)
     return
-
-
-if __name__ == "__main__":
-    # test_create()
-    # test_upload()
-    test_fetch()
-    # test_upload_replace()
