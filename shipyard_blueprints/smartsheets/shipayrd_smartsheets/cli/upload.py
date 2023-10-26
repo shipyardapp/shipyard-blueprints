602d6d5124e8f919ff25ba3c9bddefe2210af1ae
wrp801 <wespoulsen@gmail.com>
2023-10-26 14:29:47 -0600

    updated notion dependencies to updated version of shipyard templates

--- shipyard_blueprints/smartsheets/shipayrd_smartsheets/cli/upload.py
+++ shipyard_blueprints/smartsheets/shipayrd_smartsheets/cli/upload.py
@@ -3,7 +3,16 @@ import os
 import smartsheet
 import sys
 import logging
-from shipyard_templates import Spreadsheets as ss
+import pandas as pd
+from shipyard_templates import ExitCodeException, Spreadsheets as ss
+from typing import Dict, Any, List, Optional
+from dataclasses import dataclass
+
+@dataclass
+class DataRow:
+    index:int
+    values:List[smartsheet.Smartsheet.models.Row]
+
 
 
 def get_logger():
@@ -47,7 +56,44 @@ def get_args():
         default="csv",
         choices={"xlsx", "csv"},
     )
+    parser.add_argument('--insert-method', dest = 'insert_method', required = False, default = 'append', choices = {'replace','append'})
     return parser.parse_args()
+
+def form_rows_to_append(smart:smartsheet.Smartsheet,column_mapping:Dict[str,str], data:pd.DataFrame) -> List[Any]:
+    all_rows = [] # this will be a list of DataRow objects
+    columns = data.columns
+    for index in range(len(data)):
+        single_row = [] # this will be a list of smartsheet.Smartsheet.models.Row objects
+        new_row = smart.models.Row()
+        for column in columns:
+            row_value = data[column].iloc[index]
+            column_id = column_mapping.get(column)
+            new_row.to_top = False # specify that the rows will be appended to an existing sheet
+            new_row.cells.append({
+                    'column_id': column_id,
+                    'value': row_value})
+            single_row.append(new_row)
+        dr = DataRow(index, single_row)
+        # all_rows.append(dr)
+        all_rows.append(new_row)
+
+    return all_rows
+
+def upload_append(smart:smartsheet.Smartsheet, rows:List[Any], sheet_id:str):
+    try:
+        smart.Sheets.add_rows(sheet_id, rows)
+    except Exception as e:
+        raise(ExitCodeException(f'Error when trying to append rows to sheet: {str(e)}', exit_code= ss.EXIT_CODE_UPLOAD_ERROR))
+
+def upload_replace(smart:smartsheet.Smartsheet, file_path:str,name:str,file_type:str,sheet_id:Optional[str] = None):
+    # NOTE: if sheet exists, then update the rows, otherwise create a new one
+    if file_type == 'csv':
+        smart.Sheets.import_csv_sheet(file = file_path, sheet_name = name, header_row_index = 0, primary_column_index = 0)
+
+    if file_type == 'xlsx':
+        pass
+
+
 
 
 def main():
@@ -64,6 +110,13 @@ def main():
         sheet_name = args.sheet_name if args.sheet_name != "" else None
         if connect(logger, smart) == 1:
             sys.exit(ss.EXIT_CODE_INVALID_TOKEN)
+
+        if args.insert_method == 'append':
+            # handle replace
+            pass
+
+
+
         # handle the file types
         if args.file_type == "csv":
             response = smart.Sheets.import_csv_sheet(
