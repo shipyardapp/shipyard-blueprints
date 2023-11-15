b3c660058871b6f2aec681ca1243ef051d1d210f
wrp801 <wespoulsen@gmail.com>
2023-11-14 18:51:58 -0700

    ran black

--- shipyard_blueprints/notion/shipyard_notion/cli/authtest.py
+++ shipyard_blueprints/notion/shipyard_notion/cli/authtest.py
@@ -4,14 +4,18 @@ import requests
 
 
 def main():
-    token = os.getenv('NOTION_ACCESS_TOKEN')
-    headers = {'Authorization': f'Bearer {token}',
-               'accept': 'application/json',
-               'Notion-Version': '2022-06-28'}
-    url = 'https://api.notion.com/v1/users/me'
-    resp = requests.get(url, headers = headers)
+    token = os.getenv("NOTION_ACCESS_TOKEN")
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "accept": "application/json",
+        "Notion-Version": "2022-06-28",
+    }
+    url = "https://api.notion.com/v1/users/me"
+    resp = requests.get(url, headers=headers)
     if not resp.ok:
-        print(f"Could not connect to Notion with the provided access token: {resp.text}")
+        print(
+            f"Could not connect to Notion with the provided access token: {resp.text}"
+        )
         sys.exit(1)
     sys.exit(0)
 
