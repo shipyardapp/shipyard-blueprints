5b2d29aff957e09da0a930f75862dca28571de54
wrp801 <wespoulsen@gmail.com>
2023-11-14 18:51:25 -0700

    updated notion authtest

--- shipyard_blueprints/notion/shipyard_notion/cli/authtest.py
+++ shipyard_blueprints/notion/shipyard_notion/cli/authtest.py
@@ -1,17 +1,19 @@
 import os
 import sys
-from shipyard_notion import NotionClient
+import requests
 
 
 def main():
-    try:
-        client = NotionClient(token=os.getenv("NOTION_ACCESS_TOKEN"))
-        c = client.connect()
-    except Exception as e:
-        print(f"Could not connect to Notion with the provided access token due to {e}")
+    token = os.getenv('NOTION_ACCESS_TOKEN')
+    headers = {'Authorization': f'Bearer {token}',
+               'accept': 'application/json',
+               'Notion-Version': '2022-06-28'}
+    url = 'https://api.notion.com/v1/users/me'
+    resp = requests.get(url, headers = headers)
+    if not resp.ok:
+        print(f"Could not connect to Notion with the provided access token: {resp.text}")
         sys.exit(1)
-    else:
-        sys.exit(0)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
