19bb0ebb201d5de80b50663720aa4dedaa99c78e
wrp801 <wespoulsen@gmail.com>
2023-11-29 09:57:35 -0700

    updated shipyard templates

--- shipyard-templates/shipyard_templates/cloudstorage.py
+++ shipyard-templates/shipyard_templates/cloudstorage.py
@@ -4,6 +4,14 @@ from .shipyard_logger import ShipyardLogger
 
 class CloudStorage(ABC):
     EXIT_CODE_INVALID_CREDENTIALS = 202
+    EXIT_CODE_FILE_ACCESS_ERROR = 203
+    EXIT_CODE_FOLDER_ACCESS_ERROR = 204
+    EXIT_CODE_UPLOAD_ERROR = 205
+    EXIT_CODE_DOWNLOAD_ERROR = 206
+    EXIT_CODE_FILE_NOT_FOUND = 207
+    EXIT_CODE_FOLDER_CREATION_ERROR = 208
+    EXIT_CODE_FILE_MATCH_ERROR = 210
+    EXIT_CODE_UNKNOWN_ERROR = 249
 
     def __init__(self, **kwargs) -> None:
         self.logger = ShipyardLogger().logger
