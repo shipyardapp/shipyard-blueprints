975d20615dd5335ff95bc97d8c7011192c7c521a
johnathan-rodriguez <johnathan.rodriguez@shipyardapp.com>
2023-12-08 16:56:41 -0500

    Simplified auth test and replace \n with \n which was throwing an error when attempting to json.loads()

--- shipyard_blueprints/googlesheets/shipyard_googlesheets/googlesheets.py
+++ shipyard_blueprints/googlesheets/shipyard_googlesheets/googlesheets.py
@@ -1,14 +1,5 @@
-import os
-import re
 import json
-import csv
-import tempfile
-import argparse
-import socket
-import glob
 
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
 from google.oauth2 import service_account
 from shipyard_templates import Spreadsheets
 
@@ -18,21 +9,6 @@ class GoogleSheetsClient(Spreadsheets):
         self.service_account = service_account
         super().__init__(service_account=service_account)
 
-    def _set_env_vars(self):
-        try:
-            json_credentials = json.loads(self.service_account)
-            fd, path = tempfile.mkstemp()
-            print(f"Storing json credentials temporarily at {path}")
-            with os.fdopen(fd, "w") as tmp:
-                tmp.write(self.service_account)
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
-            return path
-        except Exception as e:
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.service_account
-
     def connect(self):
-        fd = self._set_env_vars()
-        creds = service_account.Credentials.from_service_account_file(fd)
-        service = build("sheets", "v4", credentials=creds)
-        drive_service = build("drive", "v3", credentials=creds)
-        return service, drive_service
+        service_account_str = self.service_account.replace("\n", "\\n")
+        service_account.Credentials.from_service_account_info(json.loads(service_account_str))
