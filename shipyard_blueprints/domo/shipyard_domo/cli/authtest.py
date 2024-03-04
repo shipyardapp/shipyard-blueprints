a220a03b428957887b2a91a06a25688c2057169e
wrp801 <wespoulsen@gmail.com>
2024-03-04 13:28:26 -0700

    removed commented code

--- shipyard_blueprints/domo/shipyard_domo/cli/authtest.py
+++ shipyard_blueprints/domo/shipyard_domo/cli/authtest.py
@@ -22,15 +22,6 @@ def main():
         logger.info("Sucessfully connected")
         sys.exit(0)
 
-    # sys.exit(
-    #     DomoClient(
-    #         client_id=os.getenv("DOMO_CLIENT_ID"),
-    #         secret_key=os.getenv("DOMO_SECRET_KEY"),
-    #         access_token=os.getenv("DOMO_ACCESS_TOKEN"),
-    #         domo_instance=os.getenv("DOMO_INSTANCE_ID"),
-    #     ).connect()
-    # )
-
 
 if __name__ == "__main__":
     main()
