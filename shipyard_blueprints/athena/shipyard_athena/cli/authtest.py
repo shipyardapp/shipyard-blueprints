775714329aa0e4ac6acf72750553dc545ed78829
wrp801 <wespoulsen@gmail.com>
2024-03-13 09:20:45 -0600

    adjusted authtest

--- shipyard_blueprints/athena/shipyard_athena/cli/authtest.py
+++ shipyard_blueprints/athena/shipyard_athena/cli/authtest.py
@@ -1,41 +1,14 @@
-import argparse
 import os
 import sys
 
-import boto3
-from shipyard_blueprints import AthenaClient
-
-
-def get_args():
-    return {
-        "aws_access_key": os.getenv("AWS_ACCESS_KEY_ID"),
-        "aws_secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-        "region": os.getenv("AWS_DEFAULT_REGION"),
-    }
+from shipyard_athena import AthenaClient
 
 
 def main():
     athena = AthenaClient(
         os.getenv("AWS_ACCESS_KEY_ID"), os.getenv("AWS_SECRET_ACCESS_KEY")
     )
-
-    try:
-        client = boto3.client(
-            "sts",
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        )
-        response = client.get_caller_identity()
-        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-            athena.logger.info("Successfully connected to AWS Athena")
-            sys.exit(0)
-    except Exception as e:
-        athena.logger.error("Could not connect to the AWS Athena")
-        sys.exit(1)
-
-    else:
-        athena.logger.error("Could not connect to the AWS Athena")
-        sys.exit(1)
+    sys.exit(athena.verify_auth())
 
 
 if __name__ == "__main__":
---
+++ shipyard_blueprints/athena/tests/auth/test_auth.py
@@ -0,0 +1,57 @@
+import os
+import boto3
+import pytest
+import subprocess
+from shipyard_athena import AthenaClient
+from dotenv import load_dotenv, find_dotenv
+from typing import Dict, List
+
+load_dotenv(find_dotenv())
+
+
+@pytest.fixture(scope="module")
+def creds() -> Dict[str, str]:
+    return {
+        "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
+        "secret": os.getenv("AWS_SECRET_ACCESS_KEY"),
+    }
+
+
+@pytest.fixture(scope="module")
+def cmd() -> List[str]:
+    return ["python3", "./shipyard_athena/cli/authtest.py"]
+
+
+def test_good_connection(creds):
+    client = AthenaClient(
+        aws_access_key=creds["access_key"], aws_secret_key=creds["secret"]
+    )
+
+    assert client.verify_auth() == 0
+
+
+def test_bad_access_key(creds):
+    client = AthenaClient(
+        aws_access_key="bad-access-key", aws_secret_key=creds["secret"]
+    )
+
+    assert client.verify_auth() == 1
+
+
+def test_bad_secret_key(creds):
+    client = AthenaClient(
+        aws_access_key=creds["access_key"], aws_secret_key="bad_secret_key"
+    )
+    assert client.verify_auth() == 1
+
+
+def test_bad_access_and_secret_key():
+    client = AthenaClient(
+        aws_access_key="bad_access_key", aws_secret_key="bad_secret_key"
+    )
+    assert client.verify_auth() == 1
+
+
+def test_credential_test(cmd):
+    process = subprocess.run(cmd)
+    assert process.returncode == 0
