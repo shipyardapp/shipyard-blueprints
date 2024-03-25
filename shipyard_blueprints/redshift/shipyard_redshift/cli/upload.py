9cfc7193fd930e143b4d4f7d9bb4cb67d8fcd18a
wrp801 <wespoulsen@gmail.com>
2024-03-24 21:29:42 -0600

    refactor complete

--- shipyard_blueprints/redshift/shipyard_redshift/cli/upload.py
+++ shipyard_blueprints/redshift/shipyard_redshift/cli/upload.py
@@ -1,11 +1,11 @@
 import argparse
-import glob
-import os
 import re
+import sys
+import shipyard_bp_utils as shipyard
+from shipyard_templates import ShipyardLogger, Database, ExitCodeException
+from shipyard_redshift import RedshiftClient
 
-import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
+logger = ShipyardLogger.get_logger()
 
 
 def get_args():
@@ -36,25 +36,13 @@ def get_args():
     parser.add_argument(
         "--insert-method",
         dest="insert_method",
-        choices={"fail", "replace", "append"},
+        choices={"replace", "append"},
         default="append",
         required=False,
     )
-    parser.add_argument("--db-connection-url", dest="db_connection_url", required=False)
     parser.add_argument("--schema", dest="schema", required=False, default="")
     args = parser.parse_args()
 
-    if (
-        not args.db_connection_url
-        and not (args.host or args.database or args.username)
-        and not os.environ.get("DB_CONNECTION_URL")
-    ):
-        parser.error(
-            """This Blueprint requires at least one of the following to be provided:\n
-            1) --db-connection-url\n
-            2) --host, --database, and --username\n
-            3) DB_CONNECTION_URL set as environment variable"""
-        )
     if args.host and not (args.database or args.username):
         parser.error("--host requires --database and --username")
     if args.database and not (args.host or args.username):
@@ -62,54 +50,6 @@ def get_args():
     if args.username and not (args.host or args.username):
         parser.error("--username requires --host and --username")
     return args
-
-
-def create_connection_url(
-    host: str, password: str, user: str, database: str, port=5439
-):
-    url = URL.create(
-        drivername="redshift+redshift_connector",
-        host=host,
-        password=password,
-        username=user,
-        port=port,
-        database=database,
-    )
-    return url
-
-
-def find_all_local_file_names(source_folder_name):
-    """
-    Returns a list of all files that exist in the current working directory,
-    filtered by source_folder_name if provided.
-    """
-    cwd = os.getcwd()
-    cwd_extension = os.path.normpath(f"{cwd}/{source_folder_name}/**")
-    file_names = glob.glob(cwd_extension, recursive=True)
-    return [file_name for file_name in file_names if os.path.isfile(file_name)]
-
-
-def find_all_file_matches(file_names, file_name_re):
-    """
-    Return a list of all file_names that matched the regular expression.
-    """
-    matching_file_names = []
-    for file in file_names:
-        if re.search(file_name_re, file):
-            matching_file_names.append(file)
-
-    return matching_file_names
-
-
-def combine_folder_and_file_name(folder_name, file_name):
-    """
-    Combine together the provided folder_name and file_name into one path variable.
-    """
-    combined_name = os.path.normpath(
-        f'{folder_name}{"/" if folder_name else ""}{file_name}'
-    )
-
-    return combined_name
 
 
 def upload_data(
@@ -156,64 +96,61 @@ def upload_data(
 
 def main():
     args = get_args()
-    source_file_name_match_type = args.source_file_name_match_type
-    source_file_name = args.source_file_name
-    source_folder_name = args.source_folder_name
-    source_full_path = combine_folder_and_file_name(
-        folder_name=source_folder_name, file_name=source_file_name
+    match_type = args.source_file_name_match_type
+    src_file = args.source_file_name
+    src_dir = args.source_folder_name
+    src_path = shipyard.file.combine_folder_and_file_name(
+        folder_name=src_dir, file_name=src_file
     )
     table_name = args.table_name
     insert_method = args.insert_method
-    database = args.database
-    schema = args.schema
-    host = args.host
-    password = args.password
-    user = args.username
-    port = args.port
-    if schema == "":
-        schema = None
+    redshift_args = {
+        "host": args.host,
+        "user": args.user,
+        "pwd": args.password,
+        "database": args.database,
+        "port": args.port,
+        "schema": args.schema if args.schema != "" else None,
+        "url_params": args.url_parameters if args.url_parameters != "" else None,
+    }
 
-    if port in ("", None):
-        db_string = create_connection_url(
-            host=host, password=password, user=user, database=database
-        )
-    else:
-        db_string = create_connection_url(
-            host=host, password=password, user=user, database=database, port=port
-        )
-
+    redshift = RedshiftClient(**redshift_args)
     try:
-        db_connection = create_engine(db_string)
-    except Exception as e:
-        print(f"Failed to connect to database {database}")
-        raise (e)
-
-    if source_file_name_match_type == "regex_match":
-        file_names = find_all_local_file_names(source_folder_name)
-        matching_file_names = find_all_file_matches(
-            file_names, re.compile(source_file_name)
-        )
-        print(f"{len(matching_file_names)} files found. Preparing to upload...")
-
-        for index, key_name in enumerate(matching_file_names):
-            upload_data(
-                source_full_path=key_name,
-                table_name=table_name,
-                insert_method=insert_method,
-                db_connection=db_connection,
-                schema=schema,
+        if match_type == "regex_match":
+            file_names = shipyard.files.find_all_local_file_names(src_dir)
+            matching_file_names = shipyard.files.find_all_file_matches(
+                file_names, re.compile(src_file)
+            )
+            if n_matches := len(matching_file_names) == 0:
+                logger.error(f"No matches found for pattern {src_file}")
+                sys.exit(Database.EXIT_CODE_NO_FILE_MATCHES)
+            logger.info(
+                f"{len(matching_file_names)} files found. Preparing to upload..."
             )
 
-    else:
-        upload_data(
-            source_full_path=source_full_path,
-            table_name=table_name,
-            insert_method=insert_method,
-            db_connection=db_connection,
-            schema=schema,
+            for file_match in matching_file_names:
+                redshift.upload(
+                    file=file_match, table_name=table_name, insert_method=insert_method
+                )
+                if insert_method == "replace":
+                    insert_method = "append"
+            else:
+                logger.info(f"Successfully loaded all files to {table_name}")
+        else:
+            redshift.upload(
+                file=src_path, table_name=table_name, insert_method=insert_method
+            )
+    except ExitCodeException as ec:
+        logger.error(ec.message)
+        sys.exit(ec.exit_code)
+
+    except Exception as e:
+        logger.error(
+            f"An unpexpected error occurred when attempting to upload into Redshift. Message from the server reads: {e}"
         )
 
-    db_connection.dispose()
+    finally:
+        redshift.close()
 
 
 if __name__ == "__main__":
--- shipyard_blueprints/redshift/shipyard_redshift/redshift.py
+++ shipyard_blueprints/redshift/shipyard_redshift/redshift.py
@@ -1,49 +1,197 @@
+import redshift_connector
+import pandas as pd
+import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.url import URL
-import redshift_connector
-from shipyard_templates import Database
+from shipyard_templates import Database, ShipyardLogger, ExitCodeException
+from shipyard_templates.database import (
+    UploadError,
+    FetchError,
+    QueryError,
+    ConnectionError,
+)
+from typing import Optional
+
+logger = ShipyardLogger.get_logger()
 
 
 class RedshiftClient(Database):
+    CHUNKSIZE = 10_000
+    MAX_FILE_SIZE = 50_000_000
+
     def __init__(
         self,
         user: str,
         pwd: str,
-        host: str = None,
-        database: str = None,
-        port: str = None,
-        url_params: str = None,
+        host: str,
+        database: str,
+        schema: Optional[str] = None,
+        port: Optional[int] = 5439,
+        url_params: Optional[str] = None,
     ) -> None:
         self.user = user
         self.pwd = pwd
         self.host = host
         self.database = database
+        self.schema = schema
         self.port = port
         self.url_params = url_params
+        self._conn = None
         super().__init__(
             user, pwd, host=host, database=database, port=port, url_params=url_params
         )
 
+    @property
+    def conn(self):
+        if not self._conn:
+            self._conn = self.connect()
+        return self._conn
+
     def connect(self):
-        con_str = URL.create(
-            drivername="redshift+redshift_connector",
-            host=self.host,
-            password=self.pwd,
-            username=self.user,
-            port=self.port,
-            database=self.database,
-        )
-        conn = create_engine(con_str)
-        # conn = redshift_connector.connect(
-        #     host=self.host, database=self.database, user=self.user, password=self.pwd
-        # )
-        return conn
+        try:
+            con_str = URL.create(
+                drivername="redshift+redshift_connector",
+                host=self.host,
+                password=self.pwd,
+                username=self.user,
+                port=self.port,
+                database=self.database,
+            )
+            conn = create_engine(con_str).connect()
+            # conn = redshift_connector.connect(
+            #     host=self.host, database=self.database, user=self.user, password=self.pwd
+            # )
+        except Exception as e:
+            logger.error("Error in connecting to Redshift")
+            raise ConnectionError(
+                f"Error connecting to Redshift. Message from the server reads: {e}"
+            )
+        else:
+            logger.info("Successfully connected to Redshift")
+            return conn
 
     def execute_query(self, query: str):
-        pass
+        try:
+            query_text = text(query)
+            self.conn.execute(query_text)
+        except Exception as e:
+            raise QueryError(e)
+        else:
+            logger.info("Successfully executed query")
 
-    def fetch(self, query: str):
-        pass
+    def fetch(self, query: str) -> pd.DataFrame:
+        try:
+            query_text = text(query)
+            df = pd.read_sql(sql=query_text, con=self.conn)
+            logger.debug("Successfully fetched results")
+        except Exception as e:
+            raise FetchError(e)
+        else:
+            return df
 
-    def upload(self, file: str):
-        pass
+    def upload(self, file: str, table_name: str, insert_method: str = "replace"):
+        try:
+            if self.schema:
+                self.execute_query(f"CREATE SCHEMA IF NOT EXISTS {self.schema}")
+            if os.path.getsize(file) < self.MAX_FILE_SIZE:
+                df = pd.read_csv(file)
+                self.upload_df(df, table_name=table_name, insert_method=insert_method)
+            else:
+                self.upload_file(
+                    file, table_name=table_name, insert_method=insert_method
+                )
+        except ExitCodeException:
+            raise
+        except Exception as e:
+            raise UploadError(table=table_name, error_msg=e)
+
+    def read_chunks(self, query: str, dest_path: str, header: bool = True):
+        """
+        Reads data from the database in chunks and saves it to a CSV file.
+
+        Args:
+            query (TextClause): The SQL query to execute.
+            dest_path (str): The path to the destination CSV file.
+            header (bool, optional): Whether to include a header row in the CSV file. Defaults to True.
+
+        Raises:
+            ChunkDownloadError: If an error occurs while downloading the chunks.
+
+        Returns:
+            None
+        """
+        try:
+            chunksize = self.CHUNKSIZE
+            first_write = False
+            for chunk in pd.read_sql_query(query, self.conn, chunksize=chunksize):
+                if not first_write:
+                    chunk.to_csv(dest_path, mode="w", header=header, index=False)
+                    first_write = True
+                else:
+                    chunk.to_csv(dest_path, mode="a", header=False, index=False)
+        except Exception as e:
+            raise FetchError(e)
+
+    def upload_df(
+        self, df: pd.DataFrame, table_name: str, insert_method: str = "replace"
+    ):
+        """
+        Uploads a pandas DataFrame to a PostgreSQL table.
+
+        Args:
+            df (pd.DataFrame): The DataFrame to be uploaded.
+            table_name (str): The name of the table in the PostgreSQL database.
+            insert_method (str, optional): The method to use for inserting the data into the table.
+                Defaults to "replace".
+
+        Raises:
+            UploadError: If an error occurs during the upload process.
+
+        """
+        try:
+            df.to_sql(
+                table_name,
+                con=self.conn,
+                index=False,
+                if_exists=insert_method,
+                method="multi",
+            )
+        except Exception as e:
+            raise UploadError(table=table_name, error_msg=e)
+
+    def upload_file(
+        self, file_path: str, table_name: str, insert_method: str = "replace"
+    ):
+        """
+        Uploads a file to a PostgreSQL table.
+
+        Args:
+            file_path (str): The path to the file to be uploaded.
+            table_name (str): The name of the table to upload the file to.
+            insert_method (str, optional): The method to use for inserting the data into the table.
+                Defaults to "replace".
+
+        Raises:
+            UploadError: If an error occurs during the upload process.
+
+        """
+        try:
+            for index, chunk in enumerate(
+                pd.read_csv(file_path, chunksize=self.CHUNKSIZE)
+            ):
+                chunk.to_sql(
+                    table_name,
+                    con=self.conn,
+                    index=False,
+                    if_exists=insert_method,
+                    method="multi",
+                )
+                if insert_method == "replace":
+                    insert_method = "append"
+        except Exception as e:
+            raise UploadError(table=table_name, error_msg=e)
+
+    def close(self):
+        if self._conn is not None:
+            logger.info("Closing connection")
+            self._conn.close()
