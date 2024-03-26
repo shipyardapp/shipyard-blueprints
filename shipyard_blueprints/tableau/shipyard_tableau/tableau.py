ab70980330763ccd49f702d7139dffdf03a65593
johnathan-rodriguez <johnathan.rodriguez@shipyardapp.com>
2024-03-25 23:24:13 -0400

    Refactored Tableau

--- shipyard_blueprints/tableau/shipyard_tableau/tableau.py
+++ shipyard_blueprints/tableau/shipyard_tableau/tableau.py
@@ -1,6 +1,7 @@
 import tableauserverclient as tsc
-from shipyard_templates import DataVisualization
-import os
+from shipyard_templates import DataVisualization, ShipyardLogger
+
+logger = ShipyardLogger.get_logger()
 
 
 class TableauClient(DataVisualization):
@@ -14,25 +15,25 @@ class TableauClient(DataVisualization):
         super().__init__(username=username, password=password, site=site)
 
     def connect(self, sign_in_method, **kwargs):
-        if sign_in_method not in ["username_password", "access_token"]:
-            self.logger.error(f"Invalid sign in method: {sign_in_method}")
-            return 1
-        elif sign_in_method == "username_password":
-            tableau_auth = tsc.TableauAuth(
-                self.username, self.password, site_id=self.site
-            )
-        elif sign_in_method == "access_token":
-            tableau_auth = tsc.PersonalAccessTokenAuth(
-                token_name=self.username,
-                personal_access_token=self.password,
-                site_id=self.site,
-            )
         try:
+            if sign_in_method not in ["username_password", "access_token"]:
+                logger.authtest(f"Invalid sign in method: {sign_in_method}")
+                return 1
+            elif sign_in_method == "username_password":
+                tableau_auth = tsc.TableauAuth(
+                    self.username, self.password, site_id=self.site
+                )
+            elif sign_in_method == "access_token":
+                tableau_auth = tsc.PersonalAccessTokenAuth(
+                    token_name=self.username,
+                    personal_access_token=self.password,
+                    site_id=self.site,
+                )
             server = tsc.Server(self.server_url, use_server_version=True)
             server.auth.sign_in(tableau_auth)
         except Exception as error:
-            self.logger.error(f"Could not connect to Tableau: {error}")
+            logger.authtest(f"Could not connect to Tableau: {error}")
             return 1
         else:
-            self.logger.info("Successfully connected to Tableau")
+            logger.authtest("Successfully connected to Tableau")
             return 0
