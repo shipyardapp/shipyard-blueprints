c6fcfa535d4068d712d26de2bb0f4d94f82dcb22
johnathan-rodriguez <johnathan.rodriguez@shipyardapp.com>
2024-03-27 23:56:39 -0400

    Initial commit for sheets refactor

--- shipyard_blueprints/googlesheets/shipyard_googlesheets/cli/clear_data.py
+++ shipyard_blueprints/googlesheets/shipyard_googlesheets/cli/clear_data.py
@@ -1,20 +1,15 @@
-import os
-import re
-import json
-import csv
-import tempfile
 import argparse
+import json
+import os
 import socket
-import glob
 
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
-from google.oauth2 import service_account
+from shipyard_bp_utils import files as shipyard
+from shipyard_templates import ShipyardLogger
 
-SCOPES = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
+from shipyard_googlesheets import utils
+
+logger = ShipyardLogger.get_logger()
+
 socket.setdefaulttimeout(600)
 
 
@@ -35,38 +30,6 @@ def get_args():
     )
     parser.add_argument("--drive", dest="drive", default=None, required=False)
     return parser.parse_args()
-
-
-def set_environment_variables(args):
-    """
-    Set GCP credentials as environment variables if they're provided via keyword
-    arguments rather than seeded as environment variables. This will override
-    system defaults.
-    """
-    credentials = args.gcp_application_credentials
-    try:
-        json_credentials = json.loads(credentials)
-        fd, path = tempfile.mkstemp()
-        print(f"Storing json credentials temporarily at {path}")
-        with os.fdopen(fd, "w") as tmp:
-            tmp.write(credentials)
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
-        return path
-    except Exception:
-        print("Using specified json credentials file")
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials
-        return
-
-
-def clean_folder_name(folder_name):
-    """
-    Cleans folders name by removing duplicate '/' as well as leading and
-    trailing '/' characters.
-    """
-    folder_name = folder_name.strip("/")
-    if folder_name != "":
-        folder_name = os.path.normpath(folder_name)
-    return folder_name
 
 
 def clear_google_sheet(service, file_name, cell_range, spreadsheet_id, tab_name):
@@ -98,101 +61,39 @@ def clear_google_sheet(service, file_name, cell_range, spreadsheet_id, tab_name)
     except Exception as e:
         if hasattr(e, "content"):
             err_msg = json.loads(e.content)
-        else:
-            print(f"Failed to clear spreadsheet {file_name}")
-        raise (e)
-
-    print(f"{file_name} succcessfully cleared between range {cell_range}.")
-
-
-def get_shared_drive_id(service, drive):
-    """
-    Search for the drive under shared Google Drives.
-    """
-    drives = service.drives().list().execute()
-    drive_id = None
-    for _drive in drives["drives"]:
-        if _drive["name"] == drive:
-            drive_id = _drive["id"]
-    return drive_id
-
-
-def get_spreadsheet_id_by_name(drive_service, file_name, drive):
-    """
-    Attempts to get sheet id from the Google Drive Client using the
-    sheet name
-    """
-    try:
-        drive_id = None
-        if drive:
-            drive_id = get_shared_drive_id(drive_service, drive)
-
-        query = 'mimeType="application/vnd.google-apps.spreadsheet"'
-        query += f' and name = "{file_name}"'
-        if drive:
-            results = (
-                drive_service.files()
-                .list(
-                    q=str(query),
-                    supportsAllDrives=True,
-                    includeItemsFromAllDrives=True,
-                    corpora="drive",
-                    driveId=drive_id,
-                    fields="files(id, name)",
-                )
-                .execute()
+            logger.error(
+                f"Failed to clear spreadsheet. Response from Google {file_name}: {err_msg}"
             )
         else:
-            results = drive_service.files().list(q=str(query)).execute()
-        files = results["files"]
-        for _file in files:
-            return _file["id"]
-        return None
-    except Exception as e:
-        print(f"Failed to fetch spreadsheetId for {file_name}")
-        raise (e)
+            logger.errpr(f"Failed to clear spreadsheet {file_name}")
+        raise e
 
-
-def get_service(credentials):
-    """
-    Attempts to create the Google Drive Client with the associated
-    environment variables
-    """
-    try:
-        creds = service_account.Credentials.from_service_account_file(
-            credentials, scopes=SCOPES
-        )
-        service = build("sheets", "v4", credentials=creds)
-        drive_service = build("drive", "v3", credentials=creds)
-        return service, drive_service
-    except Exception as e:
-        print(f"Error accessing Google Drive with service account " f"{credentials}")
-        raise (e)
+    logger.info(f"{file_name} successfully cleared between range {cell_range}.")
 
 
 def main():
     args = get_args()
-    tmp_file = set_environment_variables(args)
-    file_name = clean_folder_name(args.file_name)
+    tmp_file = utils.set_environment_variables(args)
+    file_name = shipyard.clean_folder_name(args.file_name)
     tab_name = args.tab_name
-    cell_range = "A1:ZZZ5000000" if not args.cell_range else args.cell_range
+    cell_range = args.cell_range or "A1:ZZZ5000000"
     drive = args.drive
 
     if tmp_file:
-        service, drive_service = get_service(credentials=tmp_file)
+        service, drive_service = utils.get_service(credentials=tmp_file)
     else:
-        service, drive_service = get_service(
+        service, drive_service = utils.get_service(
             credentials=args.gcp_application_credentials
         )
 
-    spreadsheet_id = get_spreadsheet_id_by_name(
+    spreadsheet_id = utils.get_spreadsheet_id_by_name(
         drive_service=drive_service, file_name=file_name, drive=drive
     )
     if not spreadsheet_id:
         if len(file_name) >= 44:
             spreadsheet_id = file_name
         else:
-            print(f"The spreadsheet {file_name} does not exist")
+            logger.error(f"The spreadsheet {file_name} does not exist")
             raise SystemExit(1)
 
     # check if workbook exists in the spreadsheet
@@ -205,7 +106,7 @@ def main():
     )
 
     if tmp_file:
-        print(f"Removing temporary credentials file {tmp_file}")
+        logger.info(f"Removing temporary credentials file {tmp_file}")
         os.remove(tmp_file)
 
 
