260efd20f6c99e4bf67c577c6a8fb99301a06c34
wrp801 <wespoulsen@gmail.com>
2024-04-03 09:08:11 -0600

    added optional skip for all tests

--- shipyard_blueprints/portable/tests/test_auth.py
+++ shipyard_blueprints/portable/tests/test_auth.py
@@ -4,7 +4,8 @@ import pytest
 from shipyard_portable import PortableClient
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+if env_exists := os.path.exists(".env"):
+    load_dotenv(find_dotenv())
 
 
 @pytest.fixture(scope="module")
@@ -12,23 +13,27 @@ def auth():
     return ["python3", "./shipyard_portable/cli/authtest.py"]
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_good_connection():
     access_token = os.getenv("PORTABLE_API_TOKEN")
     portable = PortableClient(access_token)
     assert portable.connect() == 0
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_bad_connection():
     access_token = "bad_token"
     portable = PortableClient(access_token)
     assert portable.connect() == 1
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_authtest_good_connection(auth):
     result = subprocess.run(auth, capture_output=True)
     assert result.returncode == 0
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_authtest_bad_connection(auth, monkeypatch):
     monkeypatch.setenv("PORTABLE_API_TOKEN", "bad_token")
     result = subprocess.run(auth, capture_output=True)
--- shipyard_blueprints/portable/tests/test_errs.py
+++ shipyard_blueprints/portable/tests/test_errs.py
@@ -12,16 +12,7 @@ if env_exists := os.path.exists(".env"):
     load_dotenv(find_dotenv())
 
 
-@pytest.fixture(scope="module")
-def trigger():
-    return [
-        "python3",
-        "./shipyard_portable/cli/trigger.py",
-        "--access-token",
-        os.getenv("PORTABLE_API_TOKEN"),
-    ]
-
-
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_trigger_bad_api_key():
     client = PortableClient(access_token="bad_api_token")
     flow_id = os.getenv("PORTABLE_FLOW_1")
@@ -29,6 +20,7 @@ def test_trigger_bad_api_key():
         client.trigger_sync(flow_id=flow_id)
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_trigger_bad_flow_id():
     client = PortableClient(access_token=os.getenv("PORTABLE_API_TOKEN"))
     flow_id = "bad_flow_id"
@@ -36,6 +28,7 @@ def test_trigger_bad_flow_id():
         client.trigger_sync(flow_id=flow_id)
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_get_flow_status_bad_api_key():
     client = PortableClient(access_token="bad_api_token")
     flow_id = os.getenv("PORTABLE_FLOW_1")
@@ -43,6 +36,7 @@ def test_get_flow_status_bad_api_key():
         client.get_sync_status(flow_id=flow_id)
 
 
+@pytest.mark.skipif(not env_exists, reason="No .env file found")
 def test_get_flow_status_bad_flow_id():
     client = PortableClient(access_token=os.getenv("PORTABLE_API_TOKEN"))
     flow_id = "bad_flow_id"
