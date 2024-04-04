906c36484f78483efc820a36a89dbaf118fff09e
wrp801 <wespoulsen@gmail.com>
2024-04-04 10:06:28 -0600

    added files from other branch

--- shipyard_blueprints/redshift/shipyard_redshift/cli/execute_query.py
+++ shipyard_blueprints/redshift/shipyard_redshift/cli/execute_query.py
@@ -1,7 +1,9 @@
-from sqlalchemy import create_engine, text
 import argparse
-import os
-from sqlalchemy.engine.url import URL
+import sys
+from shipyard_templates import ExitCodeException, ShipyardLogger, Database
+from shipyard_redshift import RedshiftClient
+
+logger = ShipyardLogger.get_logger()
 
 
 def get_args():
@@ -11,22 +13,10 @@ def get_args():
     parser.add_argument("--host", dest="host", required=False)
     parser.add_argument("--database", dest="database", required=False)
     parser.add_argument("--port", dest="port", default="5439", required=False)
-    parser.add_argument("--db-connection-url", dest="db_connection_url", required=False)
     parser.add_argument("--url-parameters", dest="url_parameters", required=False)
     parser.add_argument("--query", dest="query", required=True)
     args = parser.parse_args()
 
-    if (
-        not args.db_connection_url
-        and not (args.host or args.database or args.username)
-        and not os.environ.get("DB_CONNECTION_URL")
-    ):
-        parser.error(
-            """This Blueprint requires at least one of the following to be provided:\n
-            1) --db-connection-url\n
-            2) --host, --database, and --username\n
-            3) DB_CONNECTION_URL set as environment variable"""
-        )
     if args.host and not (args.database or args.username):
         parser.error("--host requires --database and --username")
     if args.database and not (args.host or args.username):
@@ -36,56 +26,31 @@ def get_args():
     return args
 
 
-def create_connection_string(args):
-    """
-    Set the database connection string as an environment variable using the keyword arguments provided.
-    This will override system defaults.
-    """
-    if args.db_connection_url:
-        os.environ["DB_CONNECTION_URL"] = args.db_connection_url
-    elif args.host and args.username and args.database:
-        os.environ["DB_CONNECTION_URL"] = (
-            f"postgresql://{args.username}:{args.password}@{args.host}:{args.port}/{args.database}?{args.url_parameters}"
-        )
-
-    db_string = os.environ.get("DB_CONNECTION_URL")
-    return db_string
-
-
-def create_connection_url(
-    host: str, password: str, user: str, database: str, port=5439
-):
-    url = URL.create(
-        drivername="redshift+redshift_connector",
-        host=host,
-        password=password,
-        username=user,
-        port=port,
-        database=database,
-    )
-    return url
-
-
 def main():
     args = get_args()
-    query = text(args.query)
-    database = args.database
-    host = args.host
-    password = args.password
-    user = args.username
-    port = args.port
+    query = args.query
+    redshift_args = {
+        "host": args.host,
+        "user": args.username,
+        "pwd": args.password,
+        "database": args.database,
+        "port": args.port,
+        "url_params": args.url_parameters if args.url_parameters != "" else None,
+    }
 
-    db_string = create_connection_url(
-        host=host, password=password, user=user, database=database, port=port
-    )
+    redshift = RedshiftClient(**redshift_args)
     try:
-        db_connection = create_engine(db_string)
+        redshift.execute_query(query)
+    except ExitCodeException as ec:
+        logger.error(ec.message)
+        sys.exit(ec.exit_code)
     except Exception as e:
-        print(f"Failed to connect to database {database}")
-        raise (e)
-    db_connection.execute(query)
-    db_connection.dispose()
-    print("Your query has been successfully executed.")
+        logger.error(
+            f"An unepxected error occurred when attempting to execute query in Redshift. Message from the server reads {e}"
+        )
+        sys.exit(Database.EXIT_CODE_UNKNOWN)
+    finally:
+        redshift.close()
 
 
 if __name__ == "__main__":
