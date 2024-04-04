d4e40bcbdafa4faeeebce5ee0640176ad2033e74
johnathan-rodriguez <johnathan.rodriguez@shipyardapp.com>
2024-04-04 16:20:52 -0400

    Initial commit for http refactor

--- shipyard_blueprints/http/shipyard_http/cli/download_file.py
+++ shipyard_blueprints/http/shipyard_http/cli/download_file.py
@@ -1,8 +1,13 @@
 import argparse
-import requests
+import json
 import os
 import sys
-import json
+
+import requests
+from shipyard_bp_utils import files as shipyard
+from shipyard_templates import ShipyardLogger
+
+logger = ShipyardLogger.get_logger()
 
 
 def get_args():
@@ -11,7 +16,7 @@ def get_args():
     parser.add_argument(
         "--custom-headers", dest="custom_headers", required=False, default="{}"
     )
-    # authentication header is separated so it can be obfuscated
+    # authentication header is separated, so it can be obfuscated
     # as a password type in the Blueprint.
     parser.add_argument(
         "--authentication-headers",
@@ -31,51 +36,15 @@ def get_args():
         default="",
         required=False,
     )
-    args = parser.parse_args()
-    return args
-
-
-def combine_folder_and_file_name(folder_name, file_name):
-    """
-    Combine together the provided folder_name and file_name into one path variable.
-    """
-    combined_name = os.path.normpath(
-        f'{folder_name}{"/" if folder_name else ""}{file_name}'
-    )
-    combined_name = os.path.normpath(combined_name)
-
-    return combined_name
-
-
-def clean_folder_name(folder_name):
-    """
-    Cleans folders name by removing duplicate '/' as well as leading and trailing '/' characters.
-    """
-    folder_name = folder_name.strip("/")
-    if folder_name != "":
-        folder_name = os.path.normpath(folder_name)
-    return folder_name
-
-
-def convert_to_boolean(string):
-    """
-    Shipyard can't support passing Booleans to code, so we have to convert
-    string values to their boolean values.
-    """
-    if string in ["True", "true", "TRUE"]:
-        value = True
-    else:
-        value = False
-    return value
+    return parser.parse_args()
 
 
 def extract_filename_from_url(url):
-    file_name = url.split("/")[-1]
-    return file_name
+    return url.split("/")[-1]
 
 
 def download_file(url, destination_name, headers=None, params=None):
-    print(f"Currently downloading the file from {url}...")
+    logger.info(f"Currently downloading the file from {url}...")
     try:
         with requests.get(url, headers=headers, stream=True, params=params) as r:
             r.raise_for_status()
@@ -83,21 +52,21 @@ def download_file(url, destination_name, headers=None, params=None):
                 for chunk in r.iter_content(chunk_size=(16 * 1024 * 1024)):
                     f.write(chunk)
 
-        print(f"Successfully downloaded {url} to {destination_name}.")
+        logger.info(f"Successfully downloaded {url} to {destination_name}.")
     except requests.exceptions.HTTPError as eh:
-        print("URL returned an HTTP Error.\n", eh)
+        logger.error("URL returned an HTTP Error.\n", eh)
         sys.exit(1)
     except requests.exceptions.ConnectionError as ec:
-        print(
+        logger.error(
             "Could not connect to the URL. Check to make sure that it was typed correctly.\n",
             ec,
         )
         sys.exit(2)
     except requests.exceptions.Timeout as et:
-        print("Timed out while connecting to the URL.\n", et)
+        logger.error("Timed out while connecting to the URL.\n", et)
         sys.exit(3)
     except requests.exceptions.RequestException as e:
-        print("Unexpected error occured. Please try again.\n", e)
+        logger.error("Unexpected error occurred. Please try again.\n", e)
         sys.exit(4)
     return
 
@@ -105,11 +74,6 @@ def download_file(url, destination_name, headers=None, params=None):
 def add_to_headers(headers, key, value):
     headers[key] = value
     return headers
-
-
-def create_folder_if_dne(destination_folder_name):
-    if not os.path.exists(destination_folder_name) and (destination_folder_name != ""):
-        os.makedirs(destination_folder_name)
 
 
 def main():
@@ -125,17 +89,13 @@ def main():
         authentication_headers = json.loads("{}")
     else:
         authentication_headers = json.loads(args.authentication_headers)
-    destination_file_name = args.destination_file_name
-    if not destination_file_name:
-        destination_file_name = extract_filename_from_url(url)
-    destination_folder_name = clean_folder_name(args.destination_folder_name)
-    destination_name = combine_folder_and_file_name(
-        destination_folder_name, destination_file_name
-    )
+    destination_file_name = args.destination_file_name or extract_filename_from_url(url)
+    destination_folder_name = shipyard.clean_folder_name(args.destination_folder_name)
+    destination_name = shipyard.combine_folder_and_file_name(destination_folder_name, destination_file_name)
     headers = {}
     params = {}
 
-    create_folder_if_dne(destination_folder_name)
+    shipyard.create_folder_if_dne(destination_folder_name)
     if custom_headers:
         for key, value in custom_headers.items():
             headers = add_to_headers(headers, key, value)
@@ -145,9 +105,9 @@ def main():
 
     download_file(url, destination_name, headers, params)
     written_file_size = os.path.getsize(destination_name)
-    print(f"The downloaded file contained {written_file_size / 1000000}MB of data.")
+    logger.info(f"The downloaded file contained {written_file_size / 1000000}MB of data.")
     if written_file_size == 0:
-        print("File downloaded contained no data.")
+        logger.warning("File downloaded contained no data.")
         sys.exit(5)
 
 
