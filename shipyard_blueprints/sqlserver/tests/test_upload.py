687781b726eeaff73ff4cc469454726b13ba2537
wrp801 <wespoulsen@gmail.com>
2024-04-23 14:37:31 -0600

    added error logging to SQL Server authtest

--- shipyard_blueprints/sqlserver/tests/test_upload.py
+++ shipyard_blueprints/sqlserver/tests/test_upload.py
@@ -14,10 +14,10 @@ load_dotenv(find_dotenv())
 @pytest.fixture(scope="module")
 def creds():
     return {
-        "host": os.getenv("SQL_HOST"),
-        "pwd": os.getenv("SQL_PWD"),
-        "user": os.getenv("SQL_USER"),
-        "db": os.getenv("SQL_DB"),
+        "host": os.getenv("MSSQL_HOST"),
+        "pwd": os.getenv("MSSQL_PASSWORD"),
+        "user": os.getenv("MSSQL_USERNAME"),
+        "db": os.getenv("MSSQL_DATABASE"),
         "table": os.getenv("UP_TABLE"),
         "regex_table": os.getenv("REGEX_TABLE"),
         "new_table": os.getenv("NEW_TABLE"),
