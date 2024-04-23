b264ddefb79f6236cf69420f3d7e7be6c9f19dac
wrp801 <wespoulsen@gmail.com>
2024-04-23 14:20:24 -0600

    added error logging for motherduck

--- shipyard_blueprints/motherduck/shipyard_motherduck/cli/authtest.py
+++ shipyard_blueprints/motherduck/shipyard_motherduck/cli/authtest.py
@@ -14,8 +14,10 @@ def main():
         )  # this is necessary for the lambda runtime
         client = MotherDuckClient(os.getenv("MOTHERDUCK_TOKEN"))
         client.connect()
+        logger.info("Successfully connected to MotherDuck")
         logger.authtest("Successfully connected to MotherDuck")
     except Exception as e:
+        logger.error("Error connecting to MotherDuck, message from server: {e}")
         logger.authtest(
             f"Error connecting to MotherDuck with the provided access token. Message from server: {e}"
         )
