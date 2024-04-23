ddc8d003a0ff6181d87dfb8a23a83a633793b6ff
wrp801 <wespoulsen@gmail.com>
2024-04-23 09:21:47 -0600

    fixed bug in logging for motherduck credential test

--- shipyard_blueprints/motherduck/shipyard_motherduck/cli/authtest.py
+++ shipyard_blueprints/motherduck/shipyard_motherduck/cli/authtest.py
@@ -12,7 +12,7 @@ def main():
         client.connect()
         logger.authtest("Successfully connected to MotherDuck")
     except Exception as e:
-        logger.authtest.error(
+        logger.authtest(
             f"Error connecting to MotherDuck with the provided access token. Message from server: {e}"
         )
         sys.exit(1)
