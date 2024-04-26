ff136e4c852e8a1f7466431db052821facdffbed
johnathan-rodriguez <johnathan.rodriguez@shipyardapp.com>
2024-04-26 15:45:32 -0400

    When attempting Authtest is now tried both sign in methods if method is not specified

--- shipyard_blueprints/tableau/shipyard_tableau/cli/authtest.py
+++ shipyard_blueprints/tableau/shipyard_tableau/cli/authtest.py
@@ -1,16 +1,16 @@
 import os
 import sys
+
 from shipyard_tableau.tableau import TableauClient
 
 
 def main():
     sys.exit(
-        TableauClient(
-            os.getenv["TABLEAU_USERNAME"],
-            os.getenv["TABLEAU_PASSWORD"],
-            os.getenv["TABLEAU_SERVER_URL"],
-            os.getenv["TABLEAU_SITE_ID"],
-        ).connect(os.getenv("TABLEAU_SIGN_IN_METHOD"))
+        TableauClient(username=os.getenv("TABLEAU_USERNAME"),
+                      password=os.getenv("TABLEAU_PASSWORD"),
+                      server_url=os.getenv("TABLEAU_SERVER_URL"),
+                      site=os.getenv("TABLEAU_SITE_ID"),
+                      ).connect(sign_in_method=os.getenv("TABLEAU_SIGN_IN_METHOD"))
     )
 
 
