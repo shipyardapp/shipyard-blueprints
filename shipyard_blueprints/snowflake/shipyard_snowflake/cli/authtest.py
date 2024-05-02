57c0a03e35b246b9ea1dd2e93fbf91257c53a1cd
wrp801 <wespoulsen@gmail.com>
2024-05-02 13:50:29 -0600

    fixed bug in authtest

--- shipyard_blueprints/snowflake/shipyard_snowflake/cli/authtest.py
+++ shipyard_blueprints/snowflake/shipyard_snowflake/cli/authtest.py
@@ -1,21 +1,23 @@
 import os
 import sys
 from shipyard_snowflake import SnowflakeClient
-from shipyard_templates import ExitCodeException
+from shipyard_templates import ExitCodeException, ShipyardLogger
+
+logger = ShipyardLogger.get_logger()
 
 
 def main():
     snowflake = SnowflakeClient(
         username=os.getenv("SNOWFLAKE_USERNAME"),
-        pwd=os.getenv("SNOWFLAKE_PASSWORD"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
         account=os.getenv("SNOWFLAKE_ACCOUNT"),
     )
     try:
         conn = snowflake.connect()
-        snowflake.logger.info("Successfully connected to Snowflake")
+        logger.authtest("Successfully connected to Snowflake")
         sys.exit(0)
     except ExitCodeException as e:
-        snowflake.logger.error("Could not connect to Snowflake")
+        logger.authtest("Could not connect to Snowflake")
         sys.exit(1)
 
 
