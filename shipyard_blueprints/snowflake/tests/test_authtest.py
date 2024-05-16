e2e775c325e7f0e2572dd0a42d76a3dccaf9e50d
johnathan-rodriguez <johnathan.rodriguez@shipyardapp.com>
2024-05-16 17:16:52 -0400

    Formatted the decode helper function to also parse in direct string values for the rsa key

--- shipyard_blueprints/snowflake/tests/test_authtest.py
+++ shipyard_blueprints/snowflake/tests/test_authtest.py
@@ -1,9 +1,22 @@
+import os
 import subprocess
+
 import pytest
 from dotenv import load_dotenv, find_dotenv
-from copy import deepcopy
 
-load_dotenv(find_dotenv())
+
+@pytest.fixture(scope="module", autouse=True)
+def get_env():
+    load_dotenv(find_dotenv())
+    if any(
+            key not in os.environ
+            for key in [
+                "SNOWFLAKE_USERNAME",
+                "SNOWFLAKE_PASSWORD",
+                "SNOWFLAKE_ACCOUNT",
+            ]
+    ):
+        pytest.skip("Missing one or more required environment variables")
 
 
 @pytest.fixture(scope="module")
@@ -11,7 +24,8 @@ def auth_cmd():
     return ["python3", "shipyard_snowflake/cli/authtest.py"]
 
 
-def test_auth_success(auth_cmd):
+def test_auth_success(auth_cmd, monkeypatch):
+    monkeypatch.delenv("SNOWFLAKE_PRIVATE_KEY")
     result = subprocess.run(auth_cmd)
     assert result.returncode == 0
 
