90ec1ec97cb4a720bfd41fe77446013ba592a484
johnathan-rodriguez <132376319+johnathan-rodriguez@users.noreply.github.com>
2024-05-22 14:25:37 -0400

    Update authtest_test.py

--- shipyard_blueprints/dbt/tests/authtest_test.py
+++ shipyard_blueprints/dbt/tests/authtest_test.py
@@ -2,11 +2,10 @@ import os
 
 import pytest
 from dotenv import load_dotenv, find_dotenv
+
 from shipyard_dbt.cli.authtest import main
 
-CREDENTIALS = [
-    "DBT_API_KEY",
-]
+CREDENTIALS = ["DBT_API_KEY", "DBT_ACCOUNT_ID"]
 
 INVALID_INPUT = ["INVALID", 123, ""]
 
@@ -36,22 +35,29 @@ def test_invalid_api_key(invalid_input, monkeypatch):
 
     assert exit_code.value.code == 1
 
-# Testing to see if the account id is needed
-@pytest.mark.parametrize("invalid_input", INVALID_INPUT)
+
+@pytest.mark.parametrize("invalid_input", ["INVALID", 123])
 def test_invalid_account_id(invalid_input, monkeypatch):
+    # The account id is technically not required for a successful authtest but if it is invalid, the test should fail
     monkeypatch.setenv("DBT_ACCOUNT_ID", invalid_input)
-    with pytest.raises(SystemExit) as exit_code:
-        main()
-    assert exit_code.value.code == 0
-
-
-
-
-@pytest.mark.parametrize("missing_env", CREDENTIALS)
-def test_missing_env(missing_env, monkeypatch):
-    monkeypatch.delenv(missing_env)
     with pytest.raises(SystemExit) as exit_code:
         main()
     assert exit_code.value.code == 1
 
 
+def test_invalid_with_empty_string_account_id(monkeypatch):
+    # The account id is technically not required for a successful authtest. If it is empty, the test should still pass
+    monkeypatch.setenv("DBT_ACCOUNT_ID", "")
+    with pytest.raises(SystemExit) as exit_code:
+        main()
+    assert exit_code.value.code == 0
+
+
+@pytest.mark.parametrize("missing_env", CREDENTIALS)
+def test_missing_env(missing_env, monkeypatch):
+    # Although the account id is not required for the authtest it is required for initializing the client. If None
+    # the authtest should fail
+    monkeypatch.delenv(missing_env)
+    with pytest.raises(SystemExit) as exit_code:
+        main()
+    assert exit_code.value.code == 1
