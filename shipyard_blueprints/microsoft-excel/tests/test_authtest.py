9e524460b6a6a765889f37f06521178dfcefc59c
wrp801 <wespoulsen@gmail.com>
2024-06-25 10:49:39 -0600

    added pytest script for authtest

---
+++ shipyard_blueprints/microsoft-excel/tests/test_authtest.py
@@ -0,0 +1,35 @@
+import subprocess
+import pytest
+from dotenv import load_dotenv, find_dotenv
+
+
+if dotenv_exists := find_dotenv():
+    load_dotenv(find_dotenv())
+
+
+@pytest.fixture(scope="module")
+def auth():
+    return ["python3", "shipyard_excel/cli/authtest.py"]
+
+
+def test_authtest(auth):
+    result = subprocess.run(auth, capture_output=True)
+    assert result.returncode == 0
+
+
+def test_authtest_bad_client_id(auth, monkeypatch):
+    monkeypatch.setenv("EXCEL_CLIENT_ID", "bad_client_id")
+    result = subprocess.run(auth, capture_output=True)
+    assert result.returncode == 1
+
+
+def test_authtest_bad_client_secret(auth, monkeypatch):
+    monkeypatch.setenv("EXCEL_CLIENT_SECRET", "bad_client_secret")
+    result = subprocess.run(auth, capture_output=True)
+    assert result.returncode == 1
+
+
+def test_authtest_bad_tenant(auth, monkeypatch):
+    monkeypatch.setenv("EXCEL_TENANT", "bad_tenant")
+    result = subprocess.run(auth, capture_output=True)
+    assert result.returncode == 1
