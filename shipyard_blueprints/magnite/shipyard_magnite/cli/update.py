c5cae006a5622e3f5cd05127c6d704d9689a8a8f
wrp801 <wespoulsen@gmail.com>
2024-11-05 15:52:49 -0700

    working implementation

--- shipyard_blueprints/magnite/shipyard_magnite/cli/update.py
+++ shipyard_blueprints/magnite/shipyard_magnite/cli/update.py
@@ -4,24 +4,62 @@ import os
 
 from shipyard_magnite import MagniteClient
 from shipyard_templates import ShipyardLogger
-from shipyard_templates import errors
+from shipyard_templates import errors, ExitCodeException
 
 logger = ShipyardLogger.get_logger()
 
+EXIT_CODE_INVALID_ARGS = 200
+
 
 def get_args():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--username", required=True)
+    parser.add_argument("--password", required=True)
+    parser.add_argument("--endpoint", required=True)
+    parser.add_argument("--id", required=False, default="")
+    parser.add_argument("--file", required=False, default="")
+    parser.add_argument(
+        "--budget-value", dest="budget_value", required=False, default=""
+    )
+    parser.add_argument(
+        "--budget-period", dest="budget_period", required=False, default=""
+    )
+    parser.add_argument(
+        "--budget-pacing", dest="budget_pacing", required=False, default=""
+    )
+    return parser.parse_args()
 
 
 def main():
     try:
-        user = "wes.poulsen@pmg.com"
-        pwd = "Y3LUNJfGwNcxvfD*9V2."
-        camp_id = "74255"
-        client = MagniteClient(user, pwd)
+        args = get_args()
+        if not args.id or args.file:
+            logger.error(
+                "Either a file or an ID must be provided. For single updates, provide an ID. For bulk updates, provide a file"
+            )
+            sys.exit(EXIT_CODE_INVALID_ARGS)
+
+        if args.id and not args.budget_value:
+            logger.error("A budget value is necessary to update a single ID")
+            sys.exit(EXIT_CODE_INVALID_ARGS)
+
+        client = MagniteClient(args.username, args.password)
         client.connect()
-        data = client.read("campaigns", camp_id)
-        print(f"Data is {data}")
+
+        if args.file:
+            pass
+        elif args.id:
+            client.update_single(
+                endpoint=args.endpoint, id=args.id, budget_value=args.budget_value
+            )
+
+        else:
+            logger.error("Must provide a file or id")
+            sys.exit(EXIT_CODE_INVALID_ARGS)
+
+    except ExitCodeException as ec:
+        logger.error(ec.message)
+        sys.exit(ec.exit_code)
     except Exception as e:
         logger.error("Error in reading data from api")
         logger.error(f"Message reads: {e}")
--- shipyard_blueprints/magnite/shipyard_magnite/magnite.py
+++ shipyard_blueprints/magnite/shipyard_magnite/magnite.py
@@ -1,10 +1,10 @@
 import json
 import requests
-from shipyard_templates import DigitalAdverstising, ShipyardLogger
+from shipyard_templates import DigitalAdverstising, ShipyardLogger, ExitCodeException
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Optional, Union
 
 logger = ShipyardLogger.get_logger()
 
@@ -32,8 +32,16 @@ class MagniteClient(DigitalAdverstising):
         else:
             return 1
 
-    def update(self):
-        pass
+    def update(
+        self, id: Optional[str], file: Optional[str], budget_value: Optional[str]
+    ):
+        if id and file:
+            # TODO: replace the hardcoded error value
+            raise ExitCodeException("Both an ID and file cannot be provided", 200)
+        if id:
+            self.update_single(id, budget_value)
+        elif file:
+            self.update_multiple()
 
     def create(self, **kwargs):
         pass
@@ -49,7 +57,73 @@ class MagniteClient(DigitalAdverstising):
             id: The ID of the item to fetch
 
         """
-        endpoint_url = f"{self.base_url}/{endpoint}/{id}"
+        self._form_url(endpoint)
+        endpoint_url = f"{self.endpoint_url}/{id}"
         response = requests.get(endpoint_url, headers=self.headers)
         response.raise_for_status()
         return response.json()
+
+    def update_single(
+        self, endpoint: str, id: str, budget_value: Union[str, float, int], **kwargs
+    ):
+        """
+
+        Args:
+            endpoint:
+            id:
+            budget_value:
+            **kwargs:
+
+        Raises:
+            ExitCodeException:
+        """
+        try:
+            budget_payload = self._make_campaign_budget_payload(
+                id, budget_value, **kwargs
+            )
+            self._form_url(endpoint)
+
+            url = f"{self.endpoint_url}/{id}"
+
+            resp = requests.put(url, headers=self.headers, json=budget_payload)
+            resp.raise_for_status()
+
+            logger.info(f"Successfully updated budget for ID {id}")
+            logger.debug(f"Response code: {resp.status_code}")
+            logger.debug(f"Response data: {resp.text}")
+        except Exception as e:
+            raise ExitCodeException(f"Error in updating budget for {id}: {e}", 1)
+
+    def update_multiple(self):
+        pass
+
+    def _make_campaign_budget_payload(
+        self, campaign_id: str, budget_value: Union[str, float, int], **kwargs
+    ):
+        """
+
+        Args:
+            campaign_id:
+            budget_value:
+            **kwargs:
+
+        Returns:
+
+        """
+        return {
+            "targeting_spend_profile": {
+                "id": 224960,
+                "budgets": [
+                    {
+                        "id": 945679,
+                        "budget_value": budget_value,
+                        # "budget_pacing": kwargs.get("budget_pacing", None),
+                        # "budget_period": kwargs.get("budget_period", None),
+                        # "budget_metric": kwargs.get("budget_metric", None),
+                    }
+                ],
+            }
+        }
+
+    def _form_url(self, endpoint: str):
+        self.endpoint_url = f"{self.base_url}/{endpoint}"
